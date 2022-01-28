ff8867150d373ee53294c12c6fee872e6c120c07
Sacha Bruttin <sacha@bruttin.com>
2022-01-28 11:35:05 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Fluent;
+using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FluentRibbonTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow 
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowsViewModel();
+        }
+
+        //private static readonly DependencyPropertyKey TitleBarPropertyKey = DependencyProperty.RegisterReadOnly(nameof(TitleBar), typeof(RibbonTitleBar), typeof(MainWindow), new PropertyMetadata());
+
+        //public static readonly DependencyProperty TitleBarProperty = TitleBarPropertyKey.DependencyProperty;
+
+        //public RibbonTitleBar TitleBar
+        //{
+        //    get => (RibbonTitleBar)GetValue(TitleBarProperty);
+        //    private set => SetValue(TitleBarPropertyKey, value);
+        //}
+
+        //private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        //{
+        //    TitleBar = this.FindChild<RibbonTitleBar>("RibbonTitleBar");
+        //    TitleBar.InvalidateArrange();
+        //    TitleBar.UpdateLayout();
+        //}
+    }
+}
---
+++ MainWindowsViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FluentRibbonTest
+{
+    public class MainWindowsViewModel : ObservableRecipient
+    {
+        private bool _isTabGroupVisible;
+
+        public bool IsTabGroupVisible
+        {
+            get { return _isTabGroupVisible; }
+            set { SetProperty(ref _isTabGroupVisible, value); }
+        }
+
+        private RelayCommand? showHideTab;
+        
+        public ICommand ShowHideTab => showHideTab ??= new RelayCommand(PerformShowHideTab);
+
+        private void PerformShowHideTab()
+        {
+            IsTabGroupVisible = !IsTabGroupVisible; 
+        }
+    }
+}
